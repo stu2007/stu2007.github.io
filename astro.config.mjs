31b7f918740f037d267eb27d0151965f56dc1dd4
stu2007 <stu2613062007@uni-plovdiv.bg>
2026-08-03 18:20:30 +0300

    Configure astro config for user root domain github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 // Настройки на Astro за съвместимост с GitHub Pages (stu2007.github.io)
 export default defineConfig({
   site: "https://stu2007.github.io", // Вашият личен GitHub Pages адрес
-  base: "/",                          // Сайтът се намира в самия корен, а не в подпапка
+  base: "/.",                          // Сайтът се намира в самия корен, а не в подпапка
   integrations: [mdx(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
